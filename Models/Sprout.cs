7ae6dc07b74e2311b78cabd6d35d7343dd687ed9
Aaron Whang and Holden Clark <holden@>
2019-07-18 12:14:20 -0700

    will add plant flavor

--- Models/Sprout.cs
+++ Models/Sprout.cs
@@ -7,43 +7,85 @@ namespace GreenHouse.plant
     public int PlantWater { get; set; }
     public int PlantFood { get; set; }
     public int PlantSun { get; set; }
+    public bool PlantAlive { get; set; }
 
 
-    public Plant(int growth, int plantWater, int plantFood, int plantSUn)
+
+    public Plant()
     {
-      Growth = growth;
-      PlantWater = plantWater;
-      PlantFood = plantFood;
-      PlantSun = plantSUn;
+      Growth = 1;
+      PlantWater = 5;
+      PlantFood = 5;
+      PlantSun = 5;
+      PlantAlive = true;
     }
 
 
-    public void Plantlife()
+    public void Plantlife(string playerChoice)
     {
+      if (playerChoice == "1")
+      {
+        GiveWater();
+        Console.WriteLine("The plant feels a little more hydrated.");
+      } else if (playerChoice == "2")
+      {
+        GiveFood();
+        Console.WriteLine("The plant feels a little less hungry.");
+      } else if (playerChoice == "3")
+      {
+        GiveSun();
+        Console.WriteLine(@"The plant praises the sun. \o/");
+      } else
+      {
+        Console.WriteLine("Not a valid input. No, you don't get your turn back.");
+      }
+
+      Random random = new Random();
+      int rand = random.next(10);
+      if (rand == 7)
+      {
+        WindStorm();
+        Console.WriteLine("The winds become severe and the clouds block the sun.");
+      } else  if (rand == 8)
+      {
+        AphidAttack();
+        Console.WriteLine("Pestilence come out of the ether and saps the lifeblood of the Plant.");
+      } else if (rand == 9)
+      {
+        SlugBite();
+        Console.WriteLine("Slug chomps on Plant. That asshole.");
+      }
+      GrowPlant();
+      WinLose();
 
     }
 
     private void GrowPlant()
     {
-      PlantWater--;
-      PlantFood--;
-      PlantSun--;
-      Growth++;
+      if (PlantFood > 0 && PlantWater > 0 && PlantSun > 0)
+      {
+        PlantWater--;
+        PlantFood--;
+        PlantSun--;
+        Growth++;
+      } else {
+        Growth--;
+      }
     }
 
     private void GiveWater()
     {
-      PlantWater += 3;
+      PlantWater += 4;
     }
 
     private void GiveFood()
     {
-      PlantFood += 3;
+      PlantFood += 4;
     }
 
     private void GiveSun()
     {
-      PlantSun += 3;
+      PlantSun += 4;
     }
 
     private void WindStorm()
@@ -61,6 +103,16 @@ namespace GreenHouse.plant
     {
       growth--;
     }
+
+    private void WinLose()
+    {
+      if (Growth <= 0)
+      {
+        PlantAlive = false;
+      }
+    }
+
+
 
 
 
---
+++ Plant.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Adventure.Branch;
+
+namespace Adventure
+{
+  public class Program
+  {
+
+    public static void Main()
+    {
+      
+
+
+
+
+
+
+
+
+
+    }
+  }
+}
