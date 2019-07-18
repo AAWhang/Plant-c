c5af9aa1f5acbaadabef0349afd8ea2a25513548
Aaron Whang and Holden Clark <holden@>
2019-07-18 13:04:25 -0700

    It will add the game

--- Models/Sprout.cs
+++ Models/Sprout.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace GreenHouse.plant
 {
   class Plants
@@ -11,7 +13,7 @@ namespace GreenHouse.plant
 
 
 
-    public Plant()
+    public Plants()
     {
       Growth = 1;
       PlantWater = 5;
@@ -23,38 +25,49 @@ namespace GreenHouse.plant
 
     public void Plantlife(string playerChoice)
     {
-      if (playerChoice == "1")
-      {
-        GiveWater();
-        Console.WriteLine("The plant feels a little more hydrated.");
-      } else if (playerChoice == "2")
-      {
-        GiveFood();
-        Console.WriteLine("The plant feels a little less hungry.");
-      } else if (playerChoice == "3")
-      {
-        GiveSun();
-        Console.WriteLine(@"The plant praises the sun. \o/");
-      } else
-      {
-        Console.WriteLine("Not a valid input. No, you don't get your turn back.");
-      }
+      Console.ForegroundColor = ConsoleColor.Green;
+        if (playerChoice == "1")
+        {
+          GiveWater();
+          Console.WriteLine("The plant feels a little more hydrated.");
+        } else if (playerChoice == "2")
+        {
+          GiveFood();
+          Console.WriteLine("The plant feels a little less hungry.");
+        } else if (playerChoice == "3")
+        {
+          GiveSun();
+          Console.WriteLine(@"The plant praises the sun. \o/");
+        } else if (playerChoice == "4")
+        {
+          Console.WriteLine("Growth Level: " + Growth);
+          Console.WriteLine("Water Level: " + PlantWater);
+          Console.WriteLine("Food Level: " + PlantFood);
+          Console.WriteLine("Sun Level: " + PlantSun);
+        } else
+        {
+          Console.ForegroundColor = ConsoleColor.Yellow;
+          Console.WriteLine("Not a valid input. No, you don't get your turn back.");
+        }
+      Console.ResetColor();
 
       Random random = new Random();
-      int rand = random.next(10);
-      if (rand == 7)
-      {
-        WindStorm();
-        Console.WriteLine("The winds become severe and the clouds block the sun.");
-      } else  if (rand == 8)
-      {
-        AphidAttack();
-        Console.WriteLine("Pestilence come out of the ether and saps the lifeblood of the Plant.");
-      } else if (rand == 9)
-      {
-        SlugBite();
-        Console.WriteLine("Slug chomps on Plant. That asshole.");
-      }
+      int rand = random.Next(10);
+      Console.ForegroundColor = ConsoleColor.Red;
+        if (rand == 7)
+        {
+          WindStorm();
+          Console.WriteLine("The winds become severe and the clouds block the sun.");
+        } else  if (rand == 8)
+        {
+          AphidAttack();
+          Console.WriteLine("Pestilence come out of the ether and saps the lifeblood of the Plant.");
+        } else if (rand == 9)
+        {
+          SlugBite();
+          Console.WriteLine("Slug chomps on Plant. That asshole.");
+        }
+      Console.ResetColor();
       GrowPlant();
       WinLose();
 
@@ -90,18 +103,18 @@ namespace GreenHouse.plant
 
     private void WindStorm()
     {
-      PlantSun--;
+      PlantSun -= 3;
     }
 
     private void AphidAttack()
     {
-      PlantFood--;
-      PlantWater--;
+      PlantFood -= 3;
+      PlantWater -= 3;
     }
 
     private void SlugBite()
     {
-      growth--;
+      Growth -= 3;
     }
 
     private void WinLose()
--- Plant.cs
+++ Plant.cs
@@ -1,15 +1,32 @@
 using System;
 using System.Collections.Generic;
-using Adventure.Branch;
+using GreenHouse.plant;
 
-namespace Adventure
+namespace Greenhouse
 {
   public class Program
   {
 
     public static void Main()
     {
-      
+      Plants Ficus = new Plants();
+      string userInput;
+        Console.WriteLine("You have bought a Plant.");
+
+      while (Ficus.PlantAlive == true) {
+        Console.WriteLine("What would you like to do? Please type in the corresponding number.");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("( 1. Water plant 2. Feed plant 3. Give plant some sun 4. Check Plant status)");
+        Console.ResetColor();
+        Console.WriteLine();
+        userInput = Console.ReadLine();
+        Ficus.Plantlife(userInput);
+      }
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine("Game Over. Your Plant is dead. Hint: buying a plant is futile. It always dies.");
+      Console.ResetColor();
+
 
 
 
